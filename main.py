664c221c09997b3391ee84b4af292cd5acc12383
Apolo2501 <77000594+Apolo2501@users.noreply.github.com>
2026-08-16 14:34:18 +0200

    Add future prediction model and endpoints

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ import os
 app = FastAPI()
 
 MODEL_PATH = "models/parking_rf_model.pkl"
+MODEL_PATH_FUTURE = "models/parking_rf_model_predictor.pkl"
 
 # Cargar el modelo al arrancar el servidor
 if os.path.exists(MODEL_PATH):
-    model = joblib.load(MODEL_PATH)
+    model_now = joblib.load(MODEL_PATH)
 else:
     raise FileNotFoundError(f"Modelo no encontrado en {MODEL_PATH}")
+    
+if os.path.exists(MODEL_PATH_FUTURE):
+    model_future = joblib.load(MODEL_PATH_FUTURE)
+else:
+    raise FileNotFoundError(f"Modelo no encontrado en {MODEL_PATH_FUTURE}")
 
-@app.post("/predict")
-def predict(data: dict):
+@app.post("/predict_now")
+def predict_now(data: dict):
     df = pd.DataFrame([data])
-    pred = model.predict(df)[0]
+    pred = model_now.predict(df)[0]
+    return {"prediction": float(pred)}
+
+@app.post("/predict_future")
+def predict_future(features: dict):
+    df = pd.DataFrame([data])
+    pred = model_future.predict(df)[0]
     return {"prediction": float(pred)}
